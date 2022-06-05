ca45f17cc1f910be6175e07c17c1e3f718438dcc
Ermel Laxamana <Ermel.Laxamana@filamsoftware.com>
2022-06-06 03:11:16 +0800

    first submission

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const mySecret = process.env['MONGO_URI']
 require('dotenv').config();
 const mongoose = require('mongoose');
 const connection = require('./mongodb.js');
+const bodyParser = require('body-parser');
 
 connection.START_CONNECTION();
 
@@ -13,6 +14,16 @@ connection.START_CONNECTION();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+
+let urlSchema = new mongoose.Schema({
+  url: {type: String , require: true},
+  short_url : Number,
+})
+
+let URL = mongoose.model('url', urlSchema)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -26,8 +37,34 @@ app.get('/api/hello', function(req, res) {
 });
 
 app.post("/api/shorturl", function (req, res){
+  let dataUrl = req.body.url;
+  let response = {};
 
-  res.json({});
+  let short_url = 1;
+  URL.findOne({url : dataUrl}, function (err, urlFound) {
+    if(err) return console.log(err);
+
+    if(!urlFound){
+      let newURL = new URL({
+        url : dataUrl,
+        short_url
+      })
+      newURL.save(function (err, saveUrlFound){
+        if (err) return console.error(err);
+
+        response = {original_url : saveUrlFound.url , short_url : saveUrlFound.short_url}
+        return res.json(response);
+      })
+    }
+    else{
+      URL.findOneAndUpdate({url : dataUrl}, {short_url : urlFound.short_url + 1}, {new: true}, (err, urlUpdateFound) => {
+        if(err) return console.log(err);
+
+        response = {original_url : urlUpdateFound.url , short_url : urlUpdateFound.short_url}
+        return res.json(response);
+    })
+    }
+  })
 })
 
 app.listen(port, function() {
