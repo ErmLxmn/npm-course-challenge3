0545c4192ae22cd50ec1b183bfa34906889d192a
Ermel Laxamana <Ermel.Laxamana@filamsoftware.com>
2022-06-06 04:11:31 +0800

    added dns look up and url parser

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const connection = require('./mongodb.js');
 const bodyParser = require('body-parser');
+const dns = require('dns');
+const urlparser = require('url');
 
 connection.START_CONNECTION();
 
@@ -36,33 +38,54 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+
+
 app.post("/api/shorturl", function (req, res){
   let dataUrl = req.body.url;
   let response = {};
-
   let short_url = 1;
-  URL.findOne({url : dataUrl}, function (err, urlFound) {
-    if(err) return console.log(err);
 
-    if(!urlFound){
-      let newURL = new URL({
-        url : dataUrl,
-        short_url
-      })
-      newURL.save(function (err, saveUrlFound){
-        if (err) return console.error(err);
-
-        response = {original_url : saveUrlFound.url , short_url : saveUrlFound.short_url}
-        return res.json(response);
-      })
-    }
-    else{
-      URL.findOneAndUpdate({url : dataUrl}, {short_url : urlFound.short_url + 1}, {new: true}, (err, urlUpdateFound) => {
+  dns.lookup(urlparser.parse(dataUrl).hostname, function (err, address){
+    if(!address){
+      return res.json({ error: 'invalid url' })
+    }else{
+      URL.findOne({url : dataUrl}, function (err, urlFound) {
         if(err) return console.log(err);
 
-        response = {original_url : urlUpdateFound.url , short_url : urlUpdateFound.short_url}
-        return res.json(response);
-    })
+        if(!urlFound){
+          let newURL = new URL({
+            url : dataUrl,
+            short_url
+          })
+          newURL.save(function (err, saveUrlFound){
+            if (err) return console.error(err);
+
+            response = {original_url : saveUrlFound.url , short_url : saveUrlFound.short_url}
+            return res.json(response);
+          })
+        }
+        else{
+          URL.findOneAndUpdate({url : dataUrl}, {short_url : urlFound.short_url + 1}, {new: true}, (err, urlUpdateFound) => {
+            if(err) return console.log(err);
+
+            response = {original_url : urlUpdateFound.url , short_url : urlUpdateFound.short_url}
+            return res.json(response);
+        })
+        }
+      })
+    }
+  })
+})
+
+app.get("/api/shorturl/:short_url", function (req, res){
+  let dataUrl = req.params.short_url;
+  URL.findOne({short_url : dataUrl}, function (err, urlFound) {
+    if(err) return console.log(err);
+    if(urlFound){
+      res.redirect(urlFound.url)
+    }
+    else{
+      res.redirect("/")
     }
   })
 })
